8cccccc4c1f714cddcc78707fb493df76e62af7e
Warlock <internalmike@gmail.com>
2022-06-11 14:43:00 +0300

    ensure_no_std

---
+++ ensure_no_std/src/main.rs
@@ -0,0 +1,22 @@
+#![feature(start)]
+
+#![deny(warnings)]
+
+#![no_std]
+
+use core::panic::PanicInfo;
+use exit_no_std::exit;
+
+#[cfg(windows)]
+#[link(name="msvcrt")]
+extern { }
+
+#[panic_handler]
+pub extern fn panic(_info: &PanicInfo) -> ! {
+    exit(99)
+}
+
+#[start]
+pub fn main(_argc: isize, _argv: *const *const u8) -> isize {
+    exit(0);
+}
