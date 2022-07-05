0c111d0fba1dcc8fef74a16c8f81a29226848346
Warlock <internalmike@gmail.com>
2022-07-06 02:43:01 +0300

    dos

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,13 @@
 
 #![no_std]
 
-#[cfg(windows)]
+#[cfg(dos)]
+pub fn exit(code: u8) -> ! {
+    pc_ints::int_21h_ah_4Ch_exit(code);
+    loop { }
+}
+
+#[cfg(all(not(dos), windows))]
 pub fn exit(code: u8) -> ! {
     unsafe { winapi::um::processthreadsapi::ExitProcess(
         code as winapi::shared::minwindef::UINT
@@ -11,7 +17,7 @@ pub fn exit(code: u8) -> ! {
     loop { }
 }
 
-#[cfg(not(windows))]
+#[cfg(all(not(dos), not(windows)))]
 pub fn exit(code: u8) -> ! {
     unsafe { libc::exit(code as u16 as i16 as libc::c_int) }
 }
